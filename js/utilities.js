1a8911554405f6f98fc7cefc4b56b984fab27056
AzaxFard <azmainulhoque@gmail.com>
2024-09-24 22:48:29 +0600

    mobile respnsive done and polished

--- js/utilities.js
+++ js/utilities.js
@@ -11,10 +11,7 @@ function getTextFieldValueById(id){
 }
 
 function showSectionById(id){
-    // hide all the sections
     document.getElementById('donation-section').classList.add('hidden');
     document.getElementById('history-section').classList.add('hidden');
-
-    // show the section with the provide id as parameter
     document.getElementById(id).classList.remove('hidden');
 }
